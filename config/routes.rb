59a54edf4b63e7960161b4fabe1e74e042788c19
Valery Suprunenko <swift.x@yahoo.com>
2023-01-30 21:14:57 +0200

    Add  routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "users#index"
+  get 'transactions', to: 'transactions#index'
+  get 'merchants', to: 'merchants#index'
+  get 'admins', to: 'admins#index'
+  get 'clients', to: 'clients#index'
+
+  resources :user, :merchant, only: [:show], shallow: true do
+    resources :transactions
+  end
+
+  scope :api, module: :api, defaults: { format: :json } do
+    resources :transactions, only: [:create, :update, :show] do
+      post :create
+    end
+  end
+
+  scope :api, module: :api, defaults: { format: :json } do
+    post 'clients', to: 'clients#show'
+  end
 end
