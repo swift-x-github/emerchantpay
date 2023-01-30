c6e900f95e5b86db9996a8477a1e0ce5c1e3f55c
Valery Suprunenko <swift.x@yahoo.com>
2023-01-30 21:13:11 +0200

    Add  formats

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  #primary_abstract_class
   EMAIL_FORMAT = %r{
     \A[\p{L}0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[\p{L}0-9!#$%&'*+/=?^_‘{|}~-]+)*@
     (?:[\p{L}0-9](?:[\p{L}0-9-]*[\p{L}0-9])?\.)+[\p{L}0-9](?:[\p{L}0-9-]*[\p{L}0-9])?\z
   }ix.freeze
   USERNAME_FORMAT = /\A[\w\-]+\z/i.freeze
+  UUID_FORMAT = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.freeze
   self.abstract_class = true
 end
