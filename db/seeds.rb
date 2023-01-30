61a6f480c737547f4525c1b560894f436230d19d
Valery Suprunenko <swift.x@yahoo.com>
2023-01-30 21:14:04 +0200

    Add  seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,28 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#  movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#  Character.create(name: "Luke", movie: movies.first)
+
+
+  Client.create!(
+    customer_email: "client@yahoo.com", balance: 1000,
+    customer_phone: "0442300505"
+    )
+
+  Merchant.create!(
+    name: "merchant_hotel", balance: 1000, 
+    email: "merchant@yahoo.com", status: :active
+  )
+
+  Admin.create!(
+    name: "admin_user", balance: 1000, email: "admin@yahoo.com",
+    status: :active
+  )
+
+#   Authorize.create!(
+#     user_id: Merchant.last.id, amount: 100,
+#     customer_email: Client.last.customer_email,
+#     customer_phone: Client.last.customer_phone,
+#     notification_url: "http://localhost:3000/api/clients"
+#   )
