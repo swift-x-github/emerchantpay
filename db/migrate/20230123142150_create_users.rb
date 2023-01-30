353381680fd19614559a43f953ba9baf3fa8362a
Valery Suprunenko <swift.x@yahoo.com>
2023-01-30 21:00:55 +0200

    Cahnge migartions for users

--- db/migrate/20230123142150_create_users.rb
+++ db/migrate/20230123142150_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false
       t.text :description
+      t.float :balance
       t.string :email, null: false
       t.integer :status, default: 0, null: false
       t.integer :role, default: 1, null: false
